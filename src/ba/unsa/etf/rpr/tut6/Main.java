de20616414de500330bebd2690b787d7b236cb73
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-12-12 22:31:34 +0100

    Update sa testovima

--- src/ba/unsa/etf/rpr/tut6/Main.java
+++ src/ba/unsa/etf/rpr/tut6/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("formular.fxml"));
         primaryStage.setTitle("Formular za studenta");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
